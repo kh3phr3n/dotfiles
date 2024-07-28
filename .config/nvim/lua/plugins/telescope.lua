f39807aeee0cf4f6ae45c1437fc85969525bd4fc
kh3phr3n <kh3phr3n@nixsys.fr>
2024-07-28 18:10:35 +0200

    Add telescope

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+  'nvim-telescope/telescope.nvim',
+
+  -- Specs
+  tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim'
+  },
+
+  init = function()
+    vim.keymap.set('n', '<Leader>ff', '<Cmd>Telescope find_files<CR>')
+    vim.keymap.set('n', '<Leader>fg', '<Cmd>Telescope git_files<CR>')
+    vim.keymap.set('n', '<Leader>fh', '<Cmd>Telescope help_tags<CR>')
+  end
+}
+
