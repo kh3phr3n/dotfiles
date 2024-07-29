8c92a429d93a6aa9b70a2f03e4a25f43cdac6163
kh3phr3n <kh3phr3n@nixsys.fr>
2024-07-29 17:18:11 +0200

    Update neovide options

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -56,12 +56,11 @@ vim.opt.completeopt = { 'menuone', 'preview' }
 vim.opt.list = true
 vim.opt.listchars = { eol = '¬', tab = '▸ ', trail = '·' }
 
+-- Neovim GUI
 if vim.g.neovide then
   -- Padding
-  vim.g.neovide_padding_top = 2
   vim.g.neovide_padding_left = 2
   vim.g.neovide_padding_right = 2
-  vim.g.neovide_padding_bottom = 2
 
   -- Text Gamma and Contrast
   vim.g.neovide_text_gamma = 0.8
