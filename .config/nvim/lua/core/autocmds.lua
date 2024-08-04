38f8e7ee3489fcc7ccb5400b3b734e92d571e443
kh3phr3n <kh3phr3n@nixsys.fr>
2024-08-04 19:36:45 +0200

    Add LSP keymaps

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -6,3 +6,25 @@ vim.api.nvim_create_autocmd('Filetype', {
   pattern = { 'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript', 'lua', 'yaml', 'md', 'markdown' }
 })
 
+-- Attach LSP actions
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function()
+    -- Buffer scope
+    local opts = { buffer = true }
+
+    -- g* keymaps
+    vim.keymap.set('n', 'gh', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.keymap.set('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    vim.keymap.set('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+    vim.keymap.set('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    vim.keymap.set('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    vim.keymap.set('n', 'gs', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    vim.keymap.set('n', 'go', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+
+    -- F* keymaps
+    vim.keymap.set('n', '<F5>', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    vim.keymap.set('n', '<F6>', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    vim.keymap.set('n', '<F7>', '<Cmd>lua vim.lsp.buf.format({ async = true })<CR>', opts)
+  end
+})
+
