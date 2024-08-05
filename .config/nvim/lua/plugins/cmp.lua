d878870f38e7ad6602f5ada29c9645aca5cd0dbd
kh3phr3n <kh3phr3n@nixsys.fr>
2024-08-05 12:38:58 +0200

    Update

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -41,28 +41,25 @@ return {
         { name = 'nvim_lsp', keyword_length = 1 }
       }),
       mapping = cmp.mapping.preset.insert({
+        -- Basics
         ['<C-e>'] = cmp.mapping.abort(),
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
         -- Super-Tab like
+        ['<CR>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if luasnip.expandable() then luasnip.expand() else cmp.confirm({ select = true }) end
+          else
+            fallback()
+          end
+        end),
+
         ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.locally_jumpable(1) then
-            luasnip.jump(1)
-          else
-            fallback()
-          end
+          if cmp.visible() then cmp.select_next_item() elseif luasnip.locally_jumpable(1) then luasnip.jump(1) else fallback() end
         end, { 'i', 's' }),
+
         ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
+          if cmp.visible() then cmp.select_prev_item() elseif luasnip.locally_jumpable(-1) then luasnip.jump(-1) else fallback() end
         end, { 'i', 's' })
       })
     })
