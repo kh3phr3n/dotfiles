d0e0ab171e22dfa78ea6c9d6e1bcb02cfd54f73f
kh3phr3n <kh3phr3n@nixsys.fr>
2024-08-05 17:16:56 +0200

    Add trouble

---
+++ .config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,13 @@
+return {
+  'folke/trouble.nvim',
+
+  -- Specs
+  cmd = 'Trouble',
+
+  init = function()
+    vim.keymap.set('n', '<Leader>tD', '<Cmd>Trouble diagnostics toggle<CR>')
+    vim.keymap.set('n', '<Leader>td', '<Cmd>Trouble diagnostics toggle filter.buf=0<CR>')
+    vim.keymap.set('n', '<Leader>ts', '<Cmd>Trouble symbols toggle focus=false win.size=100<CR>')
+  end
+}
+
