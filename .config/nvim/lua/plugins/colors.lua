4bda8ff9a6b7058971ef11e3b8c75742b967e0c8
kh3phr3n <kh3phr3n@nixsys.fr>
2024-09-04 14:55:55 +0200

    Update nord.nvim colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -12,9 +12,15 @@ return {
 
       -- Custom colors
       vim.api.nvim_set_hl(0, '@module', { fg = '#CBD3E2' })
+      vim.api.nvim_set_hl(0, '@number', { fg = '#A97EA1' })
+      vim.api.nvim_set_hl(0, '@boolean', { fg = '#D79784' })
       vim.api.nvim_set_hl(0, '@variable', { fg = '#CBD3E2' })
       vim.api.nvim_set_hl(0, '@property', { fg = '#70A9A8' })
       vim.api.nvim_set_hl(0, '@variable.member', { fg = '#70A9A8' })
+
+      vim.api.nvim_set_hl(0, '@keyword', { fg = '#6B8BB3' })
+      vim.api.nvim_set_hl(0, '@keyword.return', { fg = '#EBCB8B' })
+      vim.api.nvim_set_hl(0, '@keyword.exception', { fg = '#EBCB8B' })
 
       vim.api.nvim_set_hl(0, 'BufferLineFill', { bg = '#242933' })
       vim.api.nvim_set_hl(0, 'NeoTreeFileName', { fg = '#CBD3E2' })
