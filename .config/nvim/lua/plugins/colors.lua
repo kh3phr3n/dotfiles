e4c3813e977623b3406be90288bc5a76367df19f
kh3phr3n <kh3phr3n@nixsys.fr>
2024-09-15 16:19:17 +0200

    Update Nord fg colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -11,12 +11,12 @@ return {
       vim.cmd.colorscheme('nord')
 
       -- Custom colors
-      vim.api.nvim_set_hl(0, '@module', { fg = '#CBD3E2' })
-      vim.api.nvim_set_hl(0, '@variable', { fg = '#CBD3E2' })
-      vim.api.nvim_set_hl(0, '@variable.member', { fg = '#CBD3E2' })
+      vim.api.nvim_set_hl(0, '@module', { fg = '#D2D9E6' })
+      vim.api.nvim_set_hl(0, '@variable', { fg = '#D2D9E6' })
+      vim.api.nvim_set_hl(0, '@variable.member', { fg = '#D2D9E6' })
 
       vim.api.nvim_set_hl(0, 'BufferLineFill', { bg = '#242933' })
-      vim.api.nvim_set_hl(0, 'NeoTreeFileName', { fg = '#CBD3E2' })
+      vim.api.nvim_set_hl(0, 'NeoTreeFileName', { fg = '#D2D9E6' })
       vim.api.nvim_set_hl(0, 'NeoTreeDirectoryName', { fg = '#81A1C1' })
     end
   }
