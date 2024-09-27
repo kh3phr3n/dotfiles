9379f8fa1ac573fecb2488a9be87a88aab705daf
kh3phr3n <kh3phr3n@nixsys.fr>
2024-09-27 18:18:27 +0200

    Update Neovim nord colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -10,11 +10,6 @@ return {
     init = function()
       vim.cmd.colorscheme('nord')
 
-      -- Treesitter groups
-      vim.api.nvim_set_hl(0, '@module', { fg = '#D1D8E5' })
-      vim.api.nvim_set_hl(0, '@variable', { fg = '#D1D8E5' })
-      vim.api.nvim_set_hl(0, '@variable.member', { fg = '#D1D8E5' })
-
       -- Plugin groups
       vim.api.nvim_set_hl(0, 'IblScope', { fg = '#60728A' })
       vim.api.nvim_set_hl(0, 'BufferLineFill', { bg = '#242933' })
@@ -22,7 +17,14 @@ return {
       vim.api.nvim_set_hl(0, 'NeoTreeDirectoryName', { fg = '#81A1C1' })
 
       -- Native groups
+      vim.api.nvim_set_hl(0, 'Constant', { fg = '#D1D8E5' })
       vim.api.nvim_set_hl(0, 'Folded', { fg = '#616E88', bg = '#3B4252' })
+
+      -- Treesitter groups
+      vim.api.nvim_set_hl(0, '@module', { fg = '#D1D8E5' })
+      vim.api.nvim_set_hl(0, '@variable', { fg = '#D1D8E5' })
+      vim.api.nvim_set_hl(0, '@variable.member', { fg = '#D1D8E5' })
+      vim.api.nvim_set_hl(0, '@punctuation.delimiter', { fg = '#D1D8E5' })
     end
   }
 }
