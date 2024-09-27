edc5f7a14da2685ed4a33c3eb4d89190b3880281
kh3phr3n <kh3phr3n@nixsys.fr>
2024-09-27 18:40:03 +0200

    Minor modifications related to Neovim

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -10,15 +10,15 @@ return {
     init = function()
       vim.cmd.colorscheme('nord')
 
+      -- Native groups
+      vim.api.nvim_set_hl(0, 'Constant', { fg = '#D1D8E5' })
+      vim.api.nvim_set_hl(0, 'Folded', { fg = '#616E88', bg = '#3B4252' })
+
       -- Plugin groups
       vim.api.nvim_set_hl(0, 'IblScope', { fg = '#60728A' })
       vim.api.nvim_set_hl(0, 'BufferLineFill', { bg = '#242933' })
       vim.api.nvim_set_hl(0, 'NeoTreeFileName', { fg = '#D1D8E5' })
       vim.api.nvim_set_hl(0, 'NeoTreeDirectoryName', { fg = '#81A1C1' })
-
-      -- Native groups
-      vim.api.nvim_set_hl(0, 'Constant', { fg = '#D1D8E5' })
-      vim.api.nvim_set_hl(0, 'Folded', { fg = '#616E88', bg = '#3B4252' })
 
       -- Treesitter groups
       vim.api.nvim_set_hl(0, '@module', { fg = '#D1D8E5' })
